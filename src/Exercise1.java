bb6eaa8fef197176f207d122385fd22433664bd6
wendamys <wendamys@yandex.ru>
2026-04-24 12:54:43 +0700

    add Exercise1.java

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,7 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Exercise1 {
+
     public static void main(String[] args) {
 
+
     }
+
+    public static void inputUser() {
+        Scanner scanner = new Scanner(System.in);
+        List<String> pets = new ArrayList<>();
+        int num;
+        while (true) {
+            try {
+                String userInput = scanner.nextLine();
+                num = Integer.parseInt(userInput);
+                break;
+            } catch(Exception e){
+                System.out.println("Could not parse a number. Please, try again");
+            }
+        }
+
+        while (pets.size() < num) {
+            String userInputAnimal = scanner.nextLine();
+            if (userInputAnimal.equals("dog") || userInputAnimal.equals("cat")) {
+                try {
+                    int userInputAge = scanner.nextInt();
+                    if (userInputAge <= 0) {
+                        System.out.println("Incorrect input. Age <= 0");
+                    } else {
+                        if (userInputAnimal.equals("dog")){
+                            super.
+                        }
+                        pets.add()
+                    }
+
+                } catch (Exception e) {
+                    System.out.println("Could not parse a number. Please, try again");
+                }
+            } else {
+                System.out.println("Incorrect input. Unsupported pet type");
+            }
+
+        }
+
+    }
+
+
     public abstract class Animal {
         private String name;
         private int age;
@@ -27,10 +74,19 @@ public class Exercise1 {
     }
 
     public String toString(){
-        return "Dog name = " + getName() + ", age = " + getAge()
-        return null;
+        return "Dog name = " + getName() + ", age = " + getAge();
+        }
     }
-}
+
+    public class Cat extends Animal{
+        public Cat(String name, int age){
+            super(name, age);
+        }
+
+    public String toString(){
+        return "Cat name = " + getName() + ", age = " + getAge();
+        }
+    }
 }
 
 
